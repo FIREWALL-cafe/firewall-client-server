bf483aa1e285d7f1482db77fe7167a50a9f87c22
Dan Phiffer <dan@phiffer.org>
2017-05-10 09:21:17 -0400

    :sparkles: make certified

--- Makefile
+++ Makefile
@@ -3,3 +3,8 @@ setup:
 	debian/setup-wordpress.sh
 	debian/setup-apache.sh
 	debian/setup-db.sh firewall firewall
+
+certified:
+	debian/setup-certified.sh
+	sudo debian/setup-certified-ca.sh
+	sudo debian/setup-certified-certs.sh
--- debian/setup-certified.sh
+++ debian/setup-certified.sh
@@ -5,6 +5,16 @@ WHOAMI=`python -c 'import os, sys; print os.path.realpath(sys.argv[1])' $0`
 DEBIAN=`dirname $WHOAMI`
 ROOT=`dirname $DEBIAN`
 
+if [ ! -f /home/vagrant/.gitconfig ]; then
+	# Putting this at the top so we don't fail midway through certified-ca.sh setup
+	echo "This setup script uses your .gitconfig to autodetect your email address."
+	echo "Please run:"
+	echo
+	echo "    git config --global user.email \"you@example.com\""
+	echo "    git config --global user.name \"Your Name\""
+	exit 1
+fi
+
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y install ruby-dev
