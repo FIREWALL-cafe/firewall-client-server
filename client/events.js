b54b6b70b1f752dc071ef366350f162dcaf4077e
Dan Phiffer <dan@phiffer.org>
2017-05-18 00:51:56 -0400

    :wrench: added proxy settings

--- client/events.js
+++ client/events.js
@@ -21,3 +21,22 @@ chrome.runtime.onMessage.addListener(function(message) {
         }
     });
 });
+
+chrome.proxy.settings.set({
+	value: {
+		mode: "fixed_servers",
+		rules: {
+			proxyForHttp: {
+				scheme: "socks5",
+				host: "127.0.0.1",
+				port: 8888
+			},
+			proxyForHttps: {
+				scheme: "socks5",
+				host: "127.0.0.1",
+				port: 8888
+			},
+			bypassList: ["*.google.com"]
+		}
+	}, scope: 'regular_only'
+}, function() {});
