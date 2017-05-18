818ccf00fb1b5b9c9aaf7fa1dd8b5276f840423f
rnackman <rachel.nackman@gmail.com>
2017-05-17 21:12:45 -0400

    Add tags to top section rather than sidebar.

--- wordpress-theme/header.php
+++ wordpress-theme/header.php
@@ -4,7 +4,7 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width">
 		<title><?php
-		
+
 		if ( is_home() ) {
 			bloginfo( 'name' );
 			echo ' - ';
@@ -15,34 +15,34 @@
 			echo ' - ';
 			bloginfo( 'description' );
 		}
-		
+
 		?></title>
 		<link rel="stylesheet" href="<?php
-		
+
 		echo get_stylesheet_directory_uri() . "/reset.css";
-		
+
 		?>">
 		<link rel="stylesheet" href="<?php
-		
-		
+
+
 		echo get_stylesheet_directory_uri() . "/fonts/source-sans-pro.css";
-		
+
 		?>">
 		<link rel="stylesheet" href="<?php
-		
+
 		$last_modified = filemtime(get_stylesheet_directory() . '/style.css');
 		echo get_stylesheet_uri() . "?$last_modified";
-		
+
 		?>">
 		<link rel="shortcut icon" type="image/x-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico">
 		<?php wp_head(); ?>
-		
+
 	</head>
 	<body <?php body_class(); ?>>
 		<div class="container">
 			<header>
 				<h1><a href="/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/firewall.jpg" alt="FIREWALL Internet Cafe NYC"></a></h1><?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
-			
+
 			</header>
 		</div>
 		<div class="main">
--- wordpress-theme/index.php
+++ wordpress-theme/index.php
@@ -8,30 +8,33 @@ global $post;
 	<div class="container">
 		<h2>Search Library</h2>
 		<div class="columns">
-			<div class="sidebar">
-			<!-- <h4>View searches by category</h4> -->
+			<div class="tag-navigation-container">
 				<?php fwc_library_nav_tags(); ?>
 			</div>
 			<div class="content">
-<?php while ( have_posts() ) { the_post(); ?>
-	<section id="search-<?php echo esc_attr($post->post_name); ?>" class="search">
-		<?php
-			$link = get_the_permalink();
-			$prefix = "<h2 class=\"post-title\"><a href=\"$link\">";
-			the_title($prefix, '</a></h2>');
-		?>
-		<div class="post-content">
-			<?php the_content(); ?>
+				<?php while ( have_posts() ) { the_post(); ?>
+					<section id="search-<?php echo esc_attr($post->post_name); ?>" class="search">
+						<?php
+						$link = get_the_permalink();
+						$prefix = "<h2 class=\"post-title\"><a href=\"$link\">";
+						the_title($prefix, '</a></h2>');
+						?>
+						<div class="post-content">
+							<?php the_content(); ?>
+						</div>
+						<div class="post-meta">
+							<?php fwc_post_meta(); ?>
+						</div>
+					</section>
+				<?php } ?>
+				<section id="pagination">
+					<div class="container">
+						<div class="nav-previous"><?php next_posts_link( 'Older posts' ); ?></div>
+						<div class="nav-next"><?php previous_posts_link( 'Newer posts' ); ?></div>
+					</div>
+				</section>
+			</div>
 		</div>
-		<div class="post-meta">
-			<?php fwc_post_meta(); ?>
-		</div>
-	</section>
-<?php } ?>
-<section id="pagination">
-	<div class="container">
-		<div class="nav-previous"><?php next_posts_link( 'Older posts' ); ?></div>
-		<div class="nav-next"><?php previous_posts_link( 'Newer posts' ); ?></div>
 	</div>
 </section>
 <?php
