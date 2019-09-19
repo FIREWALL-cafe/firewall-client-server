cfceadba0728bdb7f247666a20730cd46b850dd8
Peter Behr <code@peterbehr.net>
2019-09-19 11:04:53 -0700

    Streamline header sizing and image, add icons for front page explanatory steps, remove front page sidebars

--- wordpress-theme/page-home.php
+++ wordpress-theme/page-home.php
@@ -1,34 +1,23 @@
 <?php
 /*
-Template Name: Library
+Template Name: Home
 */
 
 get_header();
 the_post();
 
 ?>
+
 <section id="home">
 	<div class="container">
-		<div class="columns">
-			<div class="sidebar">
-				<?php the_field( 'sidebar_content' ); ?>
-			</div>
-			<div class="content content--wide">
-				<?php the_content(); ?>
-				<?php
-
-				while (have_rows( 'images' )) {
-					the_row();
-					$img = get_sub_field( 'image' );
-					echo "<img src=\"{$img['sizes']['large']}\" alt=\"\">";
-				}
-
-				?>
-			</div>
+		<div class="content">
+			<?php the_content(); ?>
 		</div>
-		<?php edit_post_link('Edit'); ?>
 	</div>
 </section>
+
 <?php
 
 get_footer();
+
+?>
