d5653cc0f279e0ce6d3013690244a3dae530e6fd
Peter Behr <code@peterbehr.net>
2019-09-19 10:56:09 -0700

    Hide layout header and footer on Search Archive single view

--- wordpress-theme/footer.php
+++ wordpress-theme/footer.php
@@ -1,32 +1,23 @@
+			<?php if (!is_single() or get_post_type() != 'post') { ?>
 			<footer>
 				<div class="container">
-					<p><strong>FIREWALL Internet Cafe</strong><br>
-
-						<a href="https://oslofreedomforum.com/events/2018-oslo-freedom-forum">Oslo Freedom Forum</a>: May 28–30, 2018<br>
-
-						</p>
-					<p><a href="http://instagram.com/firewallcafe" target="_blank">Instagram</a><br>
-						<a href="https://www.facebook.com/firewallcafepopup" target="_blank">Facebook </a></p>
-					<p><a href="http://www.joyceyujeanlee.com/" target="_blank">Joyce Yu-Jean Lee</a><br>
+					<p>
+						<a href="http://instagram.com/firewallcafe" target="_blank">Instagram</a><br>
+						<a href="https://www.facebook.com/firewallcafepopup" target="_blank">Facebook</a>
+					</p>
+					<p>
+						<a href="http://www.joyceyujeanlee.com/" target="_blank">Joyce Yu-Jean Lee</a><br>
 						<a href="https://phiffer.org/" target="_blank">Dan Phiffer</a><br>
 						<a href="https://www.silascutler.com/" target="_blank">Silas Cutler</a><br>
 						<a href="http://www.rachelnackman.com/" target="_blank">Rachel Nackman</a>
-						</p>
-
+					</p>
 				</div>
 			</footer>
-			<?php wp_footer(); ?>
-			<script src="<?php
-
-			echo get_stylesheet_directory_uri() . "/js/jquery-2.2.0.min.js";
-
-			?>"></script>
-			<script src="<?php
-
-			$last_modified = filemtime(get_stylesheet_directory() . '/js/fwc.js');
-			echo get_stylesheet_directory_uri() . "/js/fwc.js?$last_modified";
-
-			?>"></script>
+			<?php } ?>
+			<script src="<?php echo get_stylesheet_directory_uri().'/js/jquery-2.2.0.min.js'; ?>"></script>
+			<script src="<?php echo get_stylesheet_directory_uri().'/js/fwc.js?'.filemtime(get_stylesheet_directory().'/js/fwc.js'); ?>"></script>
 		</div>
+	<?php wp_footer(); ?>
 	</body>
 </html>
+
--- wordpress-theme/header.php
+++ wordpress-theme/header.php
@@ -1,48 +1,39 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 	<head>
-		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta charset="<?php bloginfo('charset'); ?>">
 		<meta name="viewport" content="width=device-width">
-		<title><?php
-
-		if ( is_home() ) {
-			bloginfo( 'name' );
+		<title>
+		<?php
+		if (is_home()) {
+			bloginfo('name');
 			echo ' - ';
-			bloginfo( 'description' );
+			bloginfo('description');
 		} else {
-			wp_title( '-', true, 'right' );
-			bloginfo( 'name' );
+			wp_title('-', true, 'right');
+			bloginfo('name');
 			echo ' - ';
-			bloginfo( 'description' );
+			bloginfo('description');
 		}
-
-		?></title>
-		<link rel="stylesheet" href="<?php
-
-		echo get_stylesheet_directory_uri() . "/reset.css";
-
-		?>">
-		<link rel="stylesheet" href="<?php
-
-
-		echo get_stylesheet_directory_uri() . "/fonts/source-sans-pro.css";
-
-		?>">
-		<link rel="stylesheet" href="<?php
-
-		$last_modified = filemtime(get_stylesheet_directory() . '/style.css');
-		echo get_stylesheet_uri() . "?$last_modified";
-
-		?>">
-		<link rel="shortcut icon" type="image/x-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico">
+		?>
+		</title>
+		<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/reset.css" />
+		<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/fonts/source-sans-pro.css" />
+		<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); echo '?'; echo filemtime(get_stylesheet_directory().'/style.css'); ?>">
+		<link rel="shortcut icon" type="image/x-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 		<?php wp_head(); ?>
-
 	</head>
 	<body <?php body_class(); ?>>
+		<?php if (!is_single() or get_post_type() != 'post') { ?>
 		<div class="container">
 			<header>
-				<h1><a href="/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/firewall.jpg" alt="FIREWALL Internet Cafe NYC"></a></h1><?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
-
+				<h1>
+					<a href="/" class="wordmark">
+						<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/firewall.jpg" alt="Firewall">
+					</a>
+				</h1>
+				<?php wp_nav_menu(array('theme_location' => 'header-menu')); ?>
 			</header>
 		</div>
+		<?php } ?>
 		<div class="main">
