2af94f58806cc3695a8b2624aed8347628a31c1f
Peter Behr <code@peterbehr.net>
2019-09-21 02:43:04 -0700

    Change kiosk UX to open voting page in a new window

--- client/events.js
+++ client/events.js
@@ -25,23 +25,37 @@ chrome.runtime.onMessage.addListener(function(e) {
 			}
 			if (e.permalink) {
 				var url = e.permalink + '#votes';
+				var screen_width = window.screen.width;
+				var screen_height = window.screen.height;
+				var window_width = Math.floor(0.8 * screen_width);
+				var window_height = Math.floor(0.8 * screen_height);
+				var window_top = Math.floor((screen_height - window_height) / 2);
+				var window_left = Math.floor((screen_width - window_width) / 2);
+
 				chrome.windows.create({
 					url: url,
 					type: 'normal',
-					state: 'maximized'
+					state: 'normal',
+					focused: true,
+					width: window_width,
+					height: window_height,
+					left: window_left,
+					top: window_top
 				});
-				var options = {
-					type: 'basic',
-					iconUrl: 'icons/firewall-128.png',
-					title: e.title,
-					message: e.message
-				};
-				var notification_id = url;
-				console.log('creating notification', options);
-				chrome.notifications.create(notification_id, options);
+
+				// TODO Now that voting page opens in new window, notification seems
+				// redundant, testing new UX without it
+				// var options = {
+				// 	type: 'basic',
+				// 	iconUrl: 'icons/firewall-128.png',
+				// 	title: e.title,
+				// 	message: e.message
+				// };
+				// var notification_id = url;
+				// console.log('creating notification', options);
+				// chrome.notifications.create(notification_id, options);
 			}
-		} else if (e.type == 'toggle_input' ||
-		           e.type == 'close_intro') {
+		} else if (e.type == 'toggle_input' || e.type == 'close_intro') {
 			// Rebroadcast the event to each tab
 			for (var i = 0; i < tabs.length; i++) {
 				chrome.tabs.sendMessage(tabs[i].id, e);
