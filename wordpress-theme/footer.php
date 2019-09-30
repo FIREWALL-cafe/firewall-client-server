32ad128227048795299944d89ea24fb7cf24e697
Peter Behr <code@peterbehr.net>
2019-09-30 08:46:24 +0800

    Enhance footer, including addition of icons

--- wordpress-theme/footer.php
+++ wordpress-theme/footer.php
@@ -2,16 +2,26 @@
 			<footer>
 				<div class="container">
 					<ul>
-						<li><a href="http://instagram.com/firewallcafe" target="_blank">Instagram</a></li>
-						<li><a href="https://www.facebook.com/firewallcafe" target="_blank">Facebook</a></li>
-						<li>&copy; 2019 Firewall and affiliated contributors</li>
+						<li>
+							<img class="migrate-icon-social-media" src="<?php echo get_stylesheet_directory_uri(); ?>/img/icons-social-media-instagram.svg" alt="Instagram icon">
+							<a href="http://instagram.com/firewallcafe" target="_blank">Instagram</a>
+						</li>
+						<li>
+							<img class="migrate-icon-social-media" src="<?php echo get_stylesheet_directory_uri(); ?>/img/icons-social-media-facebook.svg" alt="Facebook icon">
+							<a href="https://www.facebook.com/firewallcafe" target="_blank">Facebook</a>
+						</li>
+						<li>
+							<img class="migrate-icon-social-media" src="<?php echo get_stylesheet_directory_uri(); ?>/img/icons-social-media-youtube.svg" alt="YouTube icon">
+							<a href="https://www.youtube.com/channel/UCMTAKSSmI9iKD7a3GB1JIrA" target="_blank">YouTube</a>
+						</li>
+						<li>&copy; 2019 FIREWALL &amp; affiliates</li>
 					</p>
 				</div>
 			</footer>
 			<?php } ?>
-			<script src="<?php echo get_stylesheet_directory_uri().'/js/jquery-2.2.0.min.js'; ?>"></script>
-			<script src="<?php echo get_stylesheet_directory_uri().'/js/fwc.js?'.filemtime(get_stylesheet_directory().'/js/fwc.js'); ?>"></script>
 		</div>
+		<script src="<?php echo get_stylesheet_directory_uri().'/js/jquery-2.2.0.min.js'; ?>"></script>
+		<script src="<?php echo get_stylesheet_directory_uri().'/js/fwc.js?'.filemtime(get_stylesheet_directory().'/js/fwc.js'); ?>"></script>
 	<?php wp_footer(); ?>
 	</body>
 </html>
