130421ab47666406f313fb71f52bad98d6b8245f
Andrew Bauman <andrew.bauman1@marist.edu>
2020-04-01 14:19:40 -0400

    Update config.sql
    Updated config.sql to include renamed variable names.

--- api/config.sql
+++ api/config.sql
@@ -1,73 +1,75 @@
 --------------
 -- Deletion --
 --------------
-DROP TABLE IF EXISTS Have_Ratings;
+DROP TABLE IF EXISTS Have_Votes;
 DROP TABLE IF EXISTS Have_Images;
-DROP TABLE IF EXISTS Have_Type;
-DROP TABLE IF EXISTS Ratings;
+DROP TABLE IF EXISTS Have_Language_Pairs;
+DROP TABLE IF EXISTS Votes;
 DROP TABLE IF EXISTS Images;
-DROP TABLE IF EXISTS Languages;
-DROP TABLE IF EXISTS Queries;
+DROP TABLE IF EXISTS Language_Pairs;
+DROP TABLE IF EXISTS Searches;
 
 --------------
 -- Creation --
 --------------
 -- Queries --
-CREATE TABLE Queries (
-  query_id    serial not null,
-  queryText   text not null,
-  queryTime   timestamp not null,
-  translation text not null,
-  confidence  float not null,
-  user        text not null,
-  expo        text,
-  primary key(query_id)
+CREATE TABLE Searches (
+  search_id       serial not null,
+  search_text     text,
+  search_time     timestamp,
+  translation     text,
+  confidence      float,
+  client_name     text,
+  search_location text,
+  ip_address      text,
+  primary key(search_id)
 );
 
 -- Types --
-CREATE TABLE Types (
-  type_id     serial not null,
-  langOne     text not null,
-  langTwo     text not null,
-  primary key(lang_id)
+CREATE TABLE Language_Pairs (
+  pair_id     serial not null,
+  langOne     text,
+  langTwo     text,
+  primary key(pair_id)
 );
 
 -- Images --
 CREATE TABLE Images (
-  image_id    serial not null,
-  imageSource boolean not null,
-  imagePath   text not null,
+  image_id         serial,
+  image_source     boolean,
+  image_path       text,
+  image_metadata   text,
   primary key(image_id)
 );
 
 -- Ratings --
-CREATE TABLE Ratings (
-  rating_id   serial not null,
-  ratingName  text not null,
-  ratingDesc  text,
-  primary key(rating_id)
+CREATE TABLE Votes (
+  vote_id    serial,
+  vote_name  text,
+  vote_desc  text,
+  primary key(vote_id)
 );
 
 -- Have_Type --
-CREATE TABLE Have_Type (
-  type_id     serial not null references Types(type_id),
-  query_id    serial not null references Queries(query_id),
-  primary key(type_id, query_id)
+CREATE TABLE Have_Language_Pairs (
+  pair_id      serial not null references Language_Pairs(pair_id),
+  search_id    serial not null references Searches(search_id),
+  primary key(pair_id, search_id)
 );
 
 -- Have_Images --
 CREATE TABLE Have_Images (
-  image_id    serial not null references Images(image_id),
-  query_id    serial not null references Queries(queries_id),
-  primary key(image_id, query_id)
+  image_id     serial not null references Images(image_id),
+  search_id    serial not null references Searches(search_id),
+  primary key(image_id, search_id)
 );
 
 -- Have_Ratings --
-CREATE TABLE Have_Ratings (
-  rating_id   serial not null references Ratings(rating_id),
-  query_id    serial not null references Queries(query_id),
-  ratingTime  timestamp not null,
-  ipAddress   text not null,
-  user        text,
-  primary key(rating_id, query_id)
+CREATE TABLE Have_Votes (
+  vote_id      serial not null references Votes(vote_id),
+  search_id    serial not null references Searches(search_id),
+  vote_time    timestamp,
+  ip_address   text,
+  client_name  text,
+  primary key(vote_id, search_id)
 );
