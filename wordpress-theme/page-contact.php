85798efebd8b5a2b894affc4c1656082bc7ad094
Peter Behr <code@peterbehr.net>
2020-04-23 09:32:40 -0700

    Sync changes to fix and improve contact form

--- wordpress-theme/page-contact.php
+++ wordpress-theme/page-contact.php
@@ -5,14 +5,24 @@ Template Name: Contact
 
 $feedback_class = 'feedback';
 if (!empty($_POST['contact_name']) &&
-    !empty($_POST['contact_email']) &&
-    !empty($_POST['contact_message']) &&
-		preg_match('/.+@.+\..+/', $_POST['contact_email'])) {
+	!empty($_POST['contact_email']) &&
+	!empty($_POST['contact_message']) &&
+	preg_match('/.+@.+\..+/', $_POST['contact_email'])) {
 	$headers = "From: FIREWALL <info@firewallcafe.com>\r\n";
-	$sent = wp_mail('info@firewallcafe.com', 'FIREWALL contact form', "{$_POST['contact_name']} <{$_POST['contact_email']}> writes:\r\n\r\n{$_POST['contact_message']}", $headers);
+	$sent = wp_mail(
+		'info@firewallcafe.com',
+		'FIREWALL contact form',
+		sprintf(
+			'%s <%s> writes:\r\n\r\n%s',
+			$_POST['contact_name'],
+			$_POST['contact_email'],
+			$_POST['contact_message']
+		),
+		$headers
+	);
 	if ($sent) {
 		$feedback = 'Thank you, your message has been sent!';
-		$feedback_class .= ' feedback--ok';
+		$feedback_class .= ' feedback-ok';
 		$_POST['contact_name'] = '';
 		$_POST['contact_email'] = '';
 		$_POST['contact_message'] = '';
@@ -29,17 +39,15 @@ the_post();
 ?>
 <section id="contact">
 	<div class="container">
-		<h2>Questions?</h2>
 		<div class="columns">
 			<div class="sidebar">
-				<p>We welcome your <br> 
-					questions, comments, and <br>
-					exhibition/event proposals <br>
-					for future programming.</p> 
-		
+				<h2>Questions?</h2>
+				<p>We welcome your questions, comments, and exhibition/event proposals for future programming.</p>
+				<h2>Pop-up?</h2>
+				<p>Feel free to suggest our next pop-up location!</p>
 			</div>
 			<div class="content">
-				<form action="." method="post">
+				<form action="#" method="post" autocomplete="off">
 					<?php if (!empty($feedback)) { ?>
 						<div class="<?php echo $feedback_class; ?>">
 							<?php echo $feedback; ?>
@@ -47,27 +55,18 @@ the_post();
 					<?php } ?>
 					<label>
 						Name:
-						<input type="text" name="contact_name" value="<?php echo esc_attr( $_POST['contact_name'] ); ?>">
+						<input type="text" name="contact_name" value="<?php if (isset($_POST['contact_name'])) { echo esc_attr($_POST['contact_name']); } ?>">
 					</label>
 					<label>
 						Email:
-						<input type="email" name="contact_email" value="<?php echo esc_attr( $_POST['contact_email'] ); ?>">
+						<input type="email" name="contact_email" value="<?php if (isset($_POST['contact_email'])) { echo esc_attr($_POST['contact_email']); } ?>">
 					</label>
 					<label>
 						Message:
-						<textarea name="contact_message" rows="5" cols="80"><?php echo esc_textarea( $_POST['contact_message'] ); ?></textarea>
+						<textarea name="contact_message" rows="5" cols="80"><?php if (isset($_POST['contact_message'])) { echo esc_textarea($_POST['contact_message']); } ?></textarea>
 					</label>
 					<button type="submit">Send message</button>
 				</form>
-			</div>
-		</div>
-	</div>
-<div class="container">
-		<h2>Pop-up?</h2>
-			<div class="columns">
-				<div class="sidebar">
-					<p>Feel free to suggest our <br>
-					next pop-up location!</p>
 			</div>
 		</div>
 	</div>
@@ -75,3 +74,5 @@ the_post();
 <?php
 
 get_footer();
+
+?>
