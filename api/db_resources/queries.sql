053e65f36af4d5810aa14d6935696c0ac3014d44
Andrew Bauman <andrew.bauman1@marist.edu>
2020-05-06 16:18:30 -0400

    Queries & GET Routes
    - Wrote some queries (queries.sql & queries.js)
    - Added the cooresponding routes
    - Broke db_configuration information (and added it to .githignore) into a separate file for security reasons.
    
    Known Issues (No issues with queries, only their respective routes):
    - At the moment any route where you specify a certain value is not working (might not be parsing integers correctly, also haven't figured out how to pass strings through yet).
    - Queries with 'counts' are not working at the moment because of a weird js syntax error  (I don't think it's the COUNT functions, but who knows... the query is good though and works in the SQL shell).

--- api/db_resources/queries.sql
+++ api/db_resources/queries.sql
@@ -1,6 +1,6 @@
 --------------------------------------------------------------------------------
 -- Firewall Cafe Database Queries and Views v1.1
--- April 2020
+-- May 2020
 -- Collaboration By:
     -- Andrew Bauman (andrewbauman1)
 --------------------------------------------------------------------------------
@@ -8,157 +8,195 @@
     -- Contains the views and stored procedures for the database to be used
     -- alongside Firewall Cafe (https://github.com/dphiffer/firewall-cafe).
 --------------------------------------------------------------------------------
---------------------------------
-          -- Views --
---------------------------------
+---------------------------------
+-- Searches w/o Images & Votes --
+---------------------------------
 
--- Returns all searches with their cooresponding image paths. --
-CREATE VIEW v_SearchInfo AS
-   SELECT s.search_id, s.search_text, s.translation, s.confidence, s.client_name,
-          s.search_location, s.search_time, s.ip_address AS 'Search IP', i.image_id, 
-          i.image_source, i.image_path, v.vote_time as
-   FROM searches s INNER JOIN have_images h ON s.search_id = h.search_id
-        INNER JOIN images i ON h.image_id = i.image_id
-   GROUP BY s.search_id, i.image_id
-   ORDER BY s.search_id ASC;
+-- Individual search result without images/Votes BY ID
+   SELECT *
+   FROM Searches s
+   WHERE s.search_id = [];
+
+-- Individual search result without images/Votes BY exact search_term_initial (Case Sensitive)
+
+   SELECT *
+   FROM Searches s
+   WHERE s.search_term_initial = [];
+
+-- All search results of any kind sorted by date (ascending)
+   SELECT *
+   FROM Searches s
+   ORDER BY s.search_timestamp ASC;
+
+-- All search results of any kind sorted by date (descending)
+   SELECT *
+   FROM Searches s
+   ORDER BY s.search_timestamp DESC;
+
+-- All search results matching combined filter statement
+    -- Matching a given location
+         SELECT *
+         FROM Searches s
+         WHERE s.search_location = [];
+
+    -- Matching a given year or time range
+         SELECT *
+         FROM Searches s
+         WHERE s.search_timestamp = [];
+
+    -- Matching a given vote category(ies)
+         Select s.*, hv.*
+         FROM Searches s INNER JOIN Have_Votes hv ON s.search_id = hv.search_id
+         WHERE vote_id = [];
+
+    -- Matching a given initial language(s) (BY search_term_initial_language_code)
+         SELECT *
+         FROM Searches s
+         WHERE s.search_term_initial_language_code = [];
 
 
--- v_votes: Calls all individual vote records for searches that have votes associated with them. --
-CREATE VIEW v_Votes AS
-  SELECT searches.search_id, search_text, translation, votes.vote_name,
-         have_votes.vote_time as 'vote timestamp', have_votes.client_name as 'Voter'
-  FROM searches, have_votes, votes
-  WHERE searches.search_id = have_votes.search_id
-  AND have_votes.vote_id = votes.vote_id;
+    -- Matching a given translation language(s)  (BY search_term_translation_language_code)
+         SELECT *
+         FROM Searches s
+         WHERE s.search_term_translation_language_code = [];
 
-  -- Returns all searches with their cooresponding image paths. --
-  CREATE VIEW v_Images AS
-     SELECT s.search_id, s.search_text, s.translation, s.confidence, s.client_name,
-            s.search_location, s.search_time, i.image_id, i.image_source, i.image_path
-     FROM searches s INNER JOIN have_images h ON s.search_id = h.search_id
-          INNER JOIN images i ON h.image_id = i.image_id
-     GROUP BY s.search_id, i.image_id
-     ORDER BY s.search_id ASC;
+    -- Matching a given initial search engine
+         SELECT *
+         FROM Searches s
+         WHERE s.search_engine_initial = [];
 
--- Returns Only Vote Counts for Searches With Votes Associated with them. --
-CREATE VIEW v_VoteCounts AS
-SELECT s.search_id, s.search_text, s.translation, s.confidence,
-     COUNT(have_votes.*) total,
-     COUNT(case when vote_id = '1' then 1 end) AS Censored,
-     COUNT(case when vote_id = '2' then 1 end) AS Uncensored,
-     COUNT(case when vote_id = '3' then 1 end) AS BadTranslation,
-     COUNT(case when vote_id = '4' then 1 end) AS GoodTranslation,
-     COUNT(case when vote_id = '5' then 1 end) AS LostInTranslation,
-     COUNT(case when vote_id = '6' then 1 end) AS NSFW,
-     COUNT(case when vote_id = '7' then 1 end) AS WTF
-  FROM searches s RIGHT OUTER JOIN have_votes ON s.search_id = have_votes.search_id
+    -- Matching a given initial search engine
+         SELECT *
+         FROM Searches s
+         WHERE s.search_engine_translation = [];
+
+------------
+-- Images --
+------------
+
+-- All Image Info for All search results
+  SELECT s.*, i.*
+  FROM Searches s FULL JOIN Images i ON s.search_id = i.search_id
+
+-- Image Info w/ search for individual search result (BY search_id)
+  SELECT s.*, i.*
+  FROM Searches s FULL JOIN Images i ON s.search_id = i.search_id
+  WHERE s.search_id = [];
+
+-- Image Info for individual search result (BY search_term_initial)
+  SELECT s.*, i.*
+  FROM Searches s FULL JOIN Images i ON s.search_id = i.search_id
+  WHERE s.search_term_initial = [];
+
+-- Image Info ONLY for individual search result (BY search_id)
+  SELECT i.*
+  FROM Searches s FULL JOIN Images i ON s.search_id = i.search_id
+  WHERE s.search_id = [];
+
+
+-----------
+-- Votes --
+-----------
+
+-- All Votes with Search Info (Only contains searches with votes b/c Inner Join)
+  SELECT s.*, hv.*
+  FROM Searches s INNER JOIN Have_Votes hv ON s.search_id = hv.search_id;
+
+-- Individual vote (BY vote_id)
+  SELECT s.*, hv.*
+  FROM Searches s INNER JOIN Have_Votes hv ON s.search_id = hv.search_id;
+  WHERE hv.vote_id = []
+
+-- Consolidated Counts for each type of vote_id (for ALL SEARCHES)
+  SELECT s.*,
+    COUNT(hv.*) total,
+    COUNT(case when vote_id = '1' then 1 end) AS Censored,
+    COUNT(case when vote_id = '2' then 1 end) AS Uncensored,
+    COUNT(case when vote_id = '3' then 1 end) AS BadTranslation,
+    COUNT(case when vote_id = '4' then 1 end) AS GoodTranslation,
+    COUNT(case when vote_id = '5' then 1 end) AS LostInTranslation,
+    COUNT(case when vote_id = '6' then 1 end) AS NSFW,
+    COUNT(case when vote_id = '7' then 1 end) AS WTF
+  FROM searches s FULL OUTER JOIN have_votes hv ON s.search_id = hv.search_id
   GROUP BY s.search_id;
 
--- Returns All Searches With Vote Counts, Whether they have votes or not. --
-  CREATE VIEW v_VoteCountsAllSearches AS
-  SELECT s.search_id, s.search_text, s.translation, s.confidence,
-       COUNT(have_votes.*) total,
-       COUNT(case when vote_id = '1' then 1 end) AS Censored,
-       COUNT(case when vote_id = '2' then 1 end) AS Uncensored,
-       COUNT(case when vote_id = '3' then 1 end) AS BadTranslation,
-       COUNT(case when vote_id = '4' then 1 end) AS GoodTranslation,
-       COUNT(case when vote_id = '5' then 1 end) AS LostInTranslation,
-       COUNT(case when vote_id = '6' then 1 end) AS NSFW,
-       COUNT(case when vote_id = '7' then 1 end) AS WTF
-    FROM searches s FULL OUTER JOIN have_votes ON s.search_id = have_votes.search_id
-    GROUP BY s.search_id;
+-- Consolidated Counts for each type of vote_id (for only searches with votes)
+  SELECT s.*,
+    COUNT(hv.*) total,
+    COUNT(case when vote_id = '1' then 1 end) AS Censored,
+    COUNT(case when vote_id = '2' then 1 end) AS Uncensored,
+    COUNT(case when vote_id = '3' then 1 end) AS BadTranslation,
+    COUNT(case when vote_id = '4' then 1 end) AS GoodTranslation,
+    COUNT(case when vote_id = '5' then 1 end) AS LostInTranslation,
+    COUNT(case when vote_id = '6' then 1 end) AS NSFW,
+    COUNT(case when vote_id = '7' then 1 end) AS WTF
+  FROM searches s INNER JOIN have_votes hv ON s.search_id = hv.search_id
+  GROUP BY s.search_id;
 
 -- Returns all Censored Searches. --
-CREATE VIEW v_CensoredCount AS
-  SELECT search_id, COUNT(*) as "Censored"
-  FROM have_votes
-  WHERE vote_id = 1
-  GROUP BY search_id;
+  SELECT s.*, COUNT(*) as "censored_votes"
+  FROM searches s INNER JOIN Have_Votes hv on s.search_id = hv.search_id
+  WHERE hv.vote_id = 1
+  GROUP BY s.search_id;
 
 -- Returns all Uncensored Searches. --
-CREATE VIEW v_UncensoredCount AS
-    SELECT search_id, COUNT(*) as "Uncensored"
-    FROM have_votes
-    WHERE vote_id = 2
-    GROUP BY search_id;
+  SELECT s.*, COUNT(*) as "uncensored_votes"
+  FROM searches s INNER JOIN Have_Votes hv on s.search_id = hv.search_id
+  WHERE hv.vote_id = 2
+  GROUP BY s.search_id;
 
--- Returns all Bad Translation searches. --
-CREATE VIEW v_BadTranslationCount AS
-    SELECT search_id, COUNT(*) as "Bad Translation"
-    FROM have_votes
-    WHERE vote_id = 3
-    GROUP BY search_id;
+-- Returns all Bad Translation Searches. --
+  SELECT s.*, COUNT(*) as "bad_translation_votes"
+  FROM searches s INNER JOIN Have_Votes hv on s.search_id = hv.search_id
+  WHERE hv.vote_id = 3
+  GROUP BY s.search_id;
 
--- Returns all Good Translation searches. --
-CREATE VIEW v_GoodTranslationCount AS
-    SELECT search_id, COUNT(*) as "Good Translation"
-    FROM have_votes
-    WHERE vote_id = 4
-    GROUP BY search_id;
+-- Returns all Good Translation Searches. --
+  SELECT s.*, COUNT(*) as "good_translation_votes"
+  FROM searches s INNER JOIN Have_Votes hv on s.search_id = hv.search_id
+  WHERE hv.vote_id = 4
+  GROUP BY s.search_id;
 
--- Returns all Lost In Translation searches. --
-CREATE VIEW v_LostInTranslationCount AS
-    SELECT search_id, COUNT(*) as "Lost In Translation"
-    FROM have_votes
-    WHERE vote_id = 5
-    GROUP BY search_id;
+-- Returns all Lost In Translation Searches. --
+  SELECT s.*, COUNT(*) as "lost_in_translation_votes"
+  FROM searches s INNER JOIN Have_Votes hv on s.search_id = hv.search_id
+  WHERE hv.vote_id = 5
+  GROUP BY s.search_id;
 
--- Returns all NSFW searches. --
-CREATE VIEW v_NSFWCount AS
-    SELECT search_id, COUNT(*) as "NSFW"
-    FROM have_votes
-    WHERE vote_id = 6
-    GROUP BY search_id;
+-- Returns all NSFW Searches. --
+  SELECT s.*, COUNT(*) as "nsfw_votes"
+  FROM searches s INNER JOIN Have_Votes hv on s.search_id = hv.search_id
+  WHERE hv.vote_id = 6
+  GROUP BY s.search_id;
 
--- Returns all WTF searches. --
-CREATE VIEW v_WTFCount AS
-   SELECT search_id, COUNT(*) as "WTF"
-   FROM have_votes
-   WHERE vote_id = 7
-   GROUP BY search_id;
+-- Returns all WTF Searches. --
+  SELECT s.*, COUNT(*) as "wtf_votes"
+  FROM searches s INNER JOIN Have_Votes hv on s.search_id = hv.search_id
+  WHERE hv.vote_id = 7
+  GROUP BY s.search_id;
 
---------------------------------
-   -- Stored Procedures --
---------------------------------
+---------------
+--- Hybrids ---
+---------------
 
-------------------------------------------------------------------------
--- Get votes, given a search_id (Uses v_AllCounts NOT v_SearchCounts) --
--- Usage:
---  SELECT getVotes('2', 'results');
---  FETCH ALL FROM results;
-------------------------------------------------------------------------
-CREATE OR REPLACE FUNCTION getVotes(int, REFCURSOR) RETURNS REFCURSOR AS
-$$
-DECLARE
-    inputNumber   int       := $1;
-    resultSet     REFCURSOR := $2;
-BEGIN
-    OPEN resultSet FOR
-         SELECT *
-         FROM v_VoteCountsAllSearches
-         WHERE search_id = inputNumber;
-    RETURN resultSet;
-END;
-$$
-LANGUAGE plpgsql;
+-- All Search Results With Vote Counts & Image Info
+  SELECT s.*, i.image_id, i.image_path, i.image_source, i.image_rank,
+    COUNT(hv.*) total,
+    COUNT(case when vote_id = '1' then 1 end) AS Censored,
+    COUNT(case when vote_id = '2' then 1 end) AS Uncensored,
+    COUNT(case when vote_id = '3' then 1 end) AS BadTranslation,
+    COUNT(case when vote_id = '4' then 1 end) AS GoodTranslation,
+    COUNT(case when vote_id = '5' then 1 end) AS LostInTranslation,
+    COUNT(case when vote_id = '6' then 1 end) AS NSFW,
+    COUNT(case when vote_id = '7' then 1 end) AS WTF
+  FROM searches s FULL OUTER JOIN have_votes hv ON s.search_id = hv.search_id
+  FULL OUTER JOIN Images i on s.search_id = i.search_id
+  GROUP BY s.search_id, i.image_id, i.image_path, i.image_source, i.image_rank;
 
-------------------------------------------------------------------------
--- Get images, given a search_id --
--- Usage:
---  SELECT getVotes('2', 'results');
---  FETCH ALL FROM results;
-------------------------------------------------------------------------
-CREATE OR REPLACE FUNCTION getImages(int, REFCURSOR) RETURNS REFCURSOR AS
-$$
-DECLARE
-    inputNumber   int       := $1;
-    resultSet     REFCURSOR := $2;
-BEGIN
-    OPEN resultSet FOR
-         SELECT *
-         FROM v_Images
-         WHERE search_id = inputNumber;
-    RETURN resultSet;
-END;
-$$
-LANGUAGE plpgsql;
+
+-- ISSUE: CARTESIAN PRODUCTS: Should retrieve votes and images separately.
+
+  --SELECT s.*, i.*, hv.*
+  --FROM Searches s FULL JOIN Images i ON s.search_id = i.search_id
+  --FULL JOIN Have_Votes hv ON s.search_id = hv.search_id
+  --WHERE s.search_term_initial = [];
