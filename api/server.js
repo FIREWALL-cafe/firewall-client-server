c38c8c4448fc22431703b65cbbe1f29ee1c6a70a
Andrew Bauman <andrew.bauman1@marist.edu>
2020-05-07 14:24:06 -0400

    Neatening Routes
    Still having trouble parsing the requested input.. Query is working but still returning empty sets.
    
    Should be finished today, along with POST routes

--- api/server.js
+++ api/server.js
@@ -20,38 +20,47 @@ app.listen(port, () => {
 })
 
 
-/*Searches w/o Images & Votes*/
+/*Routes*/
+
+    /*Search Info Only*/
 app.get('/searches', db.getAllSearches)
-app.get('/searches/time_ASC', db.getAllSearchesByTimeASC) //Not sure if this is doing anything tbh
-app.get('/searches/time_DESC', db.getAllSearchesByTimeDESC) //Not sure if this is doing anything tbh
+app.get('/searches/:search_id', db.getSearchByID)
 
-app.get('/searches/search_location/:search_location', db.getSearchBySearchLocation) //may need to adjust location names to contain no spaces
-app.get('/searches/search_id/:search_id', db.getSearchByID)
-app.get('/searches/search_timestamp/:search_timestamp', db.getSearchBySearchTimestamp) //Not sure how this will work yet (if it accepts ranges)
-app.get('/searches/initial_language_code/:initial_language_code', db.getSearchMatchingInitialLanguageCode)
-app.get('/searches/translation_language_code/:translation_language_code', db.getSearchMatchingTranslationLanguageCode)
-app.get('/searches/initial_engine/:initial_engine', db.getSearchMatchingInitialEngine)
-app.get('/searches/translation_engine/:translation_engine', db.getSearchMatchingTranslationEngine)
+    /*Searches & Image Info*/
+app.get('/searches/images', db.getAllImages)
+app.get('/searches/images/:search_id', db.getImagesAndSearchBySearchID)
 
-/*Images & Search Info*/
-app.get('/images', db.getAllImages)
-app.get('/images/images_and_searches_by_search_id/:search_id', db.getImagesAndSearchBySearchID)
-app.get('/images/images_and_searches_by_search_term/:search_term', db.getImagesAndSearchBySearchTerm)
+    /*Searches & Vote Info*/
+app.get('/searches/votes', db.getAllVotes)
+app.get('/searches/votes/by_search_id/:search_id', db.getVoteBySearchID)
+app.get('/searches/votes/by_vote_id/:search_id', db.getVoteByVoteID)
+app.get('/searches/votes/censored_searches', db.getCensoredSearches)
+app.get('/searches/votes/uncensored_searches', db.getUncensoredSearches)
+app.get('/searches/votes/bad_translation_searches', db.getBadTranslationSearches)
+app.get('/searches/votes/good_translation_searches', db.getGoodTranslationSearches)
+app.get('/searches/votes/lost_in_translation_searches', db.getLostInTranslationSearches)
+app.get('/searches/votes/nsfw_searches', db.getNSFWSearches)
+app.get('/searches/votes/wtf_searches', db.getWTFSearches)
 
-/*Images Info Only*/
-app.get('/imagesOnly/:search_id', db.getImagesOnlyBySearchID)
+   /*Searches With Vote Counts*/
+//app.get('/searches/votes/counts', db.getAllSearchesWithVoteCounts)
+//app.get('/searches/votes/counts/:search_id', db.getSearchWithVoteCountsBySearchId)
 
-/*Votes & Search Info*/
-app.get('/votes', db.getAllVotes)
-app.get('/votes/vote_id/:vote_id', db.getVoteByVoteID,) //Get Individual Vote & Search Info for One Vote
-app.get('/votes/search_id/:search_id', db.getVoteBySearchID,) //Get Individual Vote & Search Info for One Vote
-//app.get('/votes/all_searches_with_counts', db.getAllSearchesWithVoteCounts)
-//app.get('/votes/all_searches_with_counts/:search_id', db.getSearchWithVoteCountsBySearchId)
-app.get('/votes/censored_searches', db.getCensoredSearches)
-app.get('/votes/uncensored_searches', db.getUncensoredSearches)
-app.get('/votes/bad_translation_searches', db.getBadTranslationSearches)
-app.get('/votes/good_translation_searches', db.getGoodTranslationSearches)
-app.get('/votes/lost_in_translation_searches', db.getLostInTranslationSearches)
-app.get('/votes/nsfw_searches', db.getNSFWSearches)
-app.get('/votes/wtf_searches', db.getWTFSearches)
+  /* All Available Information, With Vote Counts */
 //app.get('/searches/AllVoteCountsAndImages', db.getSearchesWithVoteCountsAndImageInfo)
+
+
+
+/* TO BE REMOVED -- Can filter out results after data has been returned from routes above
+*                   in order to get the desired results.
+*
+*app.get('/votes', db.getAllVotes)
+*app.get('/votes/vote_id/:vote_id', db.getVoteByVoteID,)
+*app.get('/searches/time_ASC', db.getAllSearchesByTimeASC) //Not sure if this is doing anything tbh
+*app.get('/searches/time_DESC', db.getAllSearchesByTimeDESC) //Not sure if this is doing anything tbh
+*app.get('/searches/search_location/:search_location', db.getSearchBySearchLocation) //may need to adjust location names to contain no spaces
+*app.get('/searches/search_timestamp/:search_timestamp', db.getSearchBySearchTimestamp) //Not sure how this will work yet (if it accepts ranges)
+*app.get('/searches/initial_language_code/:initial_language_code', db.getSearchMatchingInitialLanguageCode)
+*app.get('/searches/translation_language_code/:translation_language_code', db.getSearchMatchingTranslationLanguageCode)
+*app.get('/searches/initial_engine/:initial_engine', db.getSearchMatchingInitialEngine)
+*app.get('/searches/translation_engine/:translation_engine', db.getSearchMatchingTranslationEngine) */
