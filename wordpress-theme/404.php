986ddacb7b66e9ac899926d97d6accee43a41a9e
Peter Behr <code@peterbehr.net>
2020-07-27 12:46:39 -0700

    Add 404 page

---
+++ wordpress-theme/404.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Template Name: 404
+*/
+
+get_header();
+
+?>
+
+<section id="home">
+	<div class="container">
+		<div class="content">
+			The page you requested was not found.
+		</div>
+	</div>
+</section>
+
+<?php
+
+get_footer();
+
+?>
