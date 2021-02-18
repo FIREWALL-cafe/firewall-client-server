54d3b18f2eb837a3b9eed5053ae0612d86057043
Rowan Copley <rowan.copley@gmail.com>
2021-02-18 15:57:21 -0800

    group by

--- api/queries.js
+++ api/queries.js
@@ -340,7 +340,7 @@ const getSearchesByTermWithImages = (request, response) => {
     const offset = (page-1)*page_size;
     // ignore pagination for now
     const query =  `SELECT s.*, i.image_href FROM searches s
-        INNER JOIN (SELECT array_agg(image_href), search_id FROM images) i
+        INNER JOIN (SELECT array_agg(image_href), search_id FROM images GROUP BY search_id) i
         ON s.search_id = i.search_id
         WHERE s.search_term_initial = $1;`
     // const values = [term, page_size, offset];
