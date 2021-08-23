3fab21a8ad562a1fc8379b879197fbc2dd8a5e30
Rowan Copley <rowan.copley@gmail.com>
2021-08-22 23:12:21 -0700

    rewrite client from scratch; or, creation through destruction

---
+++ client/firewall-client-rewrite.js
@@ -0,0 +1,19 @@
+
+
+// declare global variables
+
+storage.get([
+  'clientId',
+  'pendingQuery', 
+  'autocompleteEnabled'
+], init)
+
+function init(storage) {
+  console.log("got storage object:", storage)
+  // set up UI
+
+  // create event listeners
+
+  // begin main() loop
+}
+
